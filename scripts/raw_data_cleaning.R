aa1177ef084b529c1520b4611c2c22fec27eefc5
Kyle Lima <klima@schoodicinstitute.org>
2024-07-09 10:43:12 -0400

    update 20240709

--- scripts/raw_data_cleaning.R
+++ scripts/raw_data_cleaning.R
@@ -15,7 +15,10 @@ library(ggmap)
 ####             Read and clean               ####
 #------------------------------------------------#
 
-rdata <- tibble(read.csv("data/refugia_export_20231102.csv"))
+rdata <- tibble(read.csv("data/refugia_export_20240709.csv"))
+
+rdata %>% 
+  distinct(Site.code)
   
   
 cleandata <- rdata %>% 
@@ -26,14 +29,16 @@ cleandata <- rdata %>%
          group.size = how.many.people.are.in.your.group.,
          crowberry.present = is.black.crowberry.present.,
          num.cells.crowberry = how.many.of.the.grid.cells.contain.crowberry.,
-         num.cells.crowberry.flowers = how.many.of.the.grid.cells.contain.crowberry.flowers.,
+         num.cells.crowberry.flowers = how.many.of.the.grid.cells.contain.crowberry.flowers.or.flower.buds.,
          num.cells.crowberry.fruits = how.many.of.the.grid.cells.contain.crowberry.fruits.,
          num.cells.crowberry.brleaves = how.many.of.the.grid.cells.contain.brown.crowberry.leaves.,
+         num.cells.crowberry.reprod = how.many.of.the.grid.cells.contain.reproductive.components.,
          cinquefoil.present = is.three.toothed.cinquefoil.present.,
          num.cells.cinquefoil = how.many.of.the.grid.cells.contain.cinquefoil.,
-         num.cells.cinquefoil.flowers = how.many.of.the.grid.cells.contain.cinquefoil.flowers.,
+         num.cells.cinquefoil.flowers = how.many.of.the.grid.cells.contain.cinquefoil.flowers.or.flower.buds.,
          num.cells.cinquefoil.fruits = how.many.of.the.grid.cells.contain.cinquefoil.fruits.,
          num.cells.cinquefoil.brleaves = how.many.of.the.grid.cells.contain.brown.cinquefoil.leaves.,
+         num.cells.cinquefoil.reprod = how.many.of.the.grid.cells.contain.reproductive.components..1,
          end.time = survey.end.time,
          crowberry.inat = did.you.take.a.picture.of.crowberry.and.upload.it.to.inaturalist.,
          cinquefoil.inat = did.you.take.a.picture.of.cinquefoil.and.upload.it.to.inaturalist.,
@@ -47,6 +52,7 @@ cleandata <- rdata %>%
          crowberry.present = tolower(crowberry.present),
          cinquefoil.present = tolower(cinquefoil.present),
          site.code = toupper(site.code),
+         site.code = paste0(site.code, site.code.1),
          site.code = str_replace(site.code, "BMSI", "BMIS"),
          site.code = str_replace(site.code, "MDIS", "BMIS"),
          site.code = str_replace(site.code, "CABZ", "CABA"),
@@ -58,8 +64,10 @@ cleandata <- rdata %>%
          site.code = str_replace(site.code, "SSHE", "SHHA"),
          site.code = str_replace(site.code, "BLHL", "BLHI"),
          grid.cell.number = as.integer(grid.cell.number)) %>% 
-  select(site.code, grid.cell.number, latitude, longitude, survey.date, year, 
-         start.time, end.time, group.size:notes, global.id) %>% 
+  select(site.code, grid.cell.number, latitude, longitude, survey.date, year,
+         start.time, end.time, group.size:num.cells.crowberry.fruits, num.cells.crowberry.reprod,
+         num.cells.crowberry.brleaves:num.cells.cinquefoil.fruits, num.cells.cinquefoil.reprod,
+         num.cells.cinquefoil.brleaves:notes, global.id) %>%
   arrange(site.code, survey.date, start.time) %>%
   mutate(grid.cell.number = ifelse(global.id == "6ba77df0-86e8-4089-87e9-b9bdf108dd1a",
                                    46, grid.cell.number))
